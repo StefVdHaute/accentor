a12805505af424348241c656d23e9529022c8ae2
stevdnha <stef.vandenhaute@ugent.be>
2020-04-05 00:11:21 +0200

    Added icon and text

--- src/accentor/Main.java
+++ src/accentor/Main.java
@@ -1,13 +1,16 @@
 package accentor;
-  
+
 import accentor.api.DataAccessContext;
 import accentor.api.DataAccessException;
 import accentor.api.HttpDataAccessProvider;
+import accentor.browser.BrowseCompanion;
 import accentor.browser.BrowseModel;
+
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.Group; 
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
@@ -15,7 +18,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
-import accentor.browser.BrowseCompanion;
 public class Main extends Application {
     private Properties accProperties;
     private HttpDataAccessProvider dap;
@@ -31,7 +33,7 @@ public class Main extends Application {
             dap = new HttpDataAccessProvider(accProperties);
             dac = dap.getDataAccessContext();
         } catch (IOException | DataAccessException e) {
-            //TODO: catch
+            //TODO: catch properly
             e.printStackTrace();
         }
     }
@@ -40,9 +42,21 @@ public class Main extends Application {
     public void start(final Stage primaryStage) throws IOException{
         fxmlLoader = new FXMLLoader(getClass().getResource("browser/browse.fxml"));
         fxmlLoader.setController(new BrowseCompanion(new BrowseModel(dac)));
-        Group root = fxmlLoader.load();
+        BorderPane root = fxmlLoader.load();
         Scene scene = new Scene(root);
+
+        //.ico support would be nice :'(
+        //This also doesn't seem to work properly with Docky on linux mint
+        primaryStage.getIcons().add(new Image("accentor/images/rippoffyX256.png"));
+        primaryStage.getIcons().add(new Image("accentor/images/rippoffyX128.png"));
+        primaryStage.getIcons().add(new Image("accentor/images/rippoffyX64.png"));
+        primaryStage.setTitle("Rippoffy, the best, worst music player.");
+
         primaryStage.setScene(scene);
         primaryStage.show();
+    }
+
+    public static void main(String[] args){//TODO: init with arg instead of properties.
+        launch(args);
     }
 }
